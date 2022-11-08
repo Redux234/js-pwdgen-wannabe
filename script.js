ebc0d512a22405d3b4c777fdc0074329a3037833
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-08 16:08:21 +0100

    log

--- script.js
+++ script.js
@@ -4,4 +4,20 @@ let colore_preferito = prompt('Inserire il colore preferito')
 let numero1 = prompt('Scegliere un numero da 1 a 100')
 let numero2 = prompt('Sceglere un altro numero')
 
+console.log(
+    `
+    -----------Informazioni------------
+    Nome: ${nome}
+    Cognome: ${cognome}
+    Colore preferito: ${colore_preferito}
+    Numero 1: ${numero1}
+    Numero 2: ${numero2}
+    `
+)
+
+
+
+
+
+
 document.getElementById("Password").innerHTML = nome + cognome + colore_preferito;
