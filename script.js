c07d1227a0aa97b38b5c967af3fccde4fbbbca1c
Redux234 <lorenzo.gaetano2002@gmail.com>
2022-11-08 15:54:23 +0100

    prompt

---
+++ script.js
@@ -0,0 +1,5 @@
+let nome = prompt("Inserire Nome")
+let cognome =prompt('Inserire Cognome')
+let colore_preferito = prompt('Inserire il colore preferito')
+let numero1 = prompt('Scegliere un numero da 1 a 100')
+let numero2 = prompt('Sceglere un altro numero')
